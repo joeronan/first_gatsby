4a885fa5c1c5ecce0b03a70cb0fe4f7cc2b580d7
joeronan <josephr409@hotmail.co.uk>
2021-04-17 00:43:11 +0100

    Changed utils to fix problem with server side rendering

--- src/components/utils.js
+++ src/components/utils.js
@@ -6,22 +6,26 @@ https://stackoverflow.com/questions/36862334/get-viewport-window-height-in-react
 
 import React from 'react';
 
-function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
-  return {
-    width,
-    height
-  };
-}
-
 export function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = React.useState(getWindowDimensions());
+  const [windowDimensions, setWindowDimensions] = React.useState({
+    width: 100,
+    height: 100
+  });
 
   React.useEffect(() => {
+    function getWindowDimensions() {
+      const { innerWidth: width, innerHeight: height } = window;
+      return {
+        width,
+        height
+      };
+    }
+
     function handleResize() {
       setWindowDimensions(getWindowDimensions());
     }
 
+    handleResize()
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, []);
